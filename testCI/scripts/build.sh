b1b97a072fc17cf9b9be5028fe42ad221596f22c
dujiepeng <347302029@qq.com>
2018-08-07 06:58:09 +0800

    18

--- testCI/scripts/build.sh
+++ testCI/scripts/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # xcodebuild -project testCI/testCI.xcodeproj -target testCI -sdk iphonesimulator ONLY_ACTIVE_ARCH=NO
-xcodebuild archive -project testCI/testCI.xcodeproj -scheme testCI -configuration Release -archivePath archive/testCI.xcarchive CODE_SIGN_IDENTITY="${DEVELOPER_NAME}" PROVISIONING_PROFILE="${PROFILE_NAME}"
+xcodebuild archive -project testCI/testCI.xcodeproj -scheme testCI -configuration Release -archivePath archive/testCI.xcarchive CODE_SIGN_IDENTITY="iPhone Distribution: EaseMob Inc." PROVISIONING_PROFILE="testProfile"
 xcodebuild -exportArchive -archivePath archive/testCI.xcarchive -exportPath build  -exportOptionsPlist ./testCI/ExportOptions.plist
