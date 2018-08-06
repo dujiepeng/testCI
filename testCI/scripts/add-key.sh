0245a06bf99b065e9af2eb9dcf341efb49e5ba06
dujiepeng <347302029@qq.com>
2018-08-07 07:13:49 +0800

    21

--- testCI/scripts/add-key.sh
+++ testCI/scripts/add-key.sh
@@ -3,7 +3,7 @@ echo */
 security create-keychain -p travis ios-build.keychain
 security import ./testCI/scripts/certs/apple.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
 security import ./testCI/scripts/certs/dist.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
-security import ./testCI/scripts/certs/dist.p12 -k ~/Library/Keychains/ios-build.keychain -P $KEY_PASSWORD -T /usr/bin/codesign
+security import ./testCI/scripts/certs/dist.p12 -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 cp ./testCI/scripts/profile/testCI.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/
 security cms -D -i ./testCI/scripts/profile/testCI.mobileprovision
