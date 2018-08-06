eb4163fcd82af978014f9acc0cb1418d2df4ebcf
dujiepeng <347302029@qq.com>
2018-08-07 07:45:44 +0800

    25

--- testCI/scripts/build.sh
+++ testCI/scripts/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # xcodebuild -project testCI/testCI.xcodeproj -target testCI -sdk iphonesimulator ONLY_ACTIVE_ARCH=NO
-xcodebuild archive -project testCI/testCI.xcodeproj -scheme testCI -configuration Release -archivePath archive/testCI.xcarchive CODE_SIGN_IDENTITY="${DEVELOPER_NAME}" PROVISIONING_PROFILE="${PROFILE_NAME}"
+xcodebuild archive -project testCI/testCI.xcodeproj -scheme testCI -configuration Release -archivePath archive/testCI.xcarchive 
 xcodebuild -exportArchive -archivePath archive/testCI.xcarchive -exportPath build  -exportOptionsPlist ./testCI/ExportOptions.plist
