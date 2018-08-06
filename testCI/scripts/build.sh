caba5725ad6e70a3e9204197be56430a2b2b41d6
dujiepeng <347302029@qq.com>
2018-08-06 18:10:14 +0800

    update build.sh

--- testCI/scripts/build.sh
+++ testCI/scripts/build.sh
@@ -1,7 +1,1 @@
-
-xcodebuild archive -workspace easemob-OA.xcworkspace -scheme easemob-OA -configuration Release -archivePath archive/easemob-OA.xcarchive CODE_SIGN_IDENTITY="iPhone Developer"
-xcodebuild -exportArchive -archivePath archive/easemob-OA.xcarchive -exportPath build  -exportOptionsPlist ./ExportOptions.plist
-
-
-
-
+xcodebuild -project testCI.xcodeproj -target testCI -sdk iphonesimulator ONLY_ACTIVE_ARCH=NO
