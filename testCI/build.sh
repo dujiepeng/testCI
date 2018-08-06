fff6a2143cb52dc1d3dedf17c6d632ff0e3a7bfc
dujiepeng <347302029@qq.com>
2018-08-07 00:13:52 +0800

    13

--- testCI/build.sh
+++ testCI/build.sh
@@ -1,7 +1,9 @@
 
-xcodebuild archive -workspace easemob-OA.xcworkspace -scheme easemob-OA -configuration Release -archivePath archive/easemob-OA.xcarchive CODE_SIGN_IDENTITY="iPhone Developer"
-xcodebuild -exportArchive -archivePath archive/easemob-OA.xcarchive -exportPath build  -exportOptionsPlist ./ExportOptions.plist
+# xcodebuild archive -workspace easemob-OA.xcworkspace -scheme easemob-OA -configuration Release -archivePath archive/easemob-OA.xcarchive CODE_SIGN_IDENTITY="iPhone Developer"
+# xcodebuild -exportArchive -archivePath archive/easemob-OA.xcarchive -exportPath build  -exportOptionsPlist ./ExportOptions.plist
 
 
 
 
+xcodebuild archive -project testCI.xcodeproj -scheme testCI -configuration Release -archivePath archive/testCI.xcarchive CODE_SIGN_IDENTITY="iPhone Developer"
+xcodebuild -exportArchive -archivePath archive/testCI.xcarchive -exportPath build  -exportOptionsPlist ./ExportOptions.plist
--- testCI/scripts/build.sh
+++ testCI/scripts/build.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-xcodebuild -project testCI/testCI.xcodeproj -target testCI -sdk iphonesimulator ONLY_ACTIVE_ARCH=NO
+# xcodebuild -project testCI/testCI.xcodeproj -target testCI -sdk iphonesimulator ONLY_ACTIVE_ARCH=NO
+xcodebuild archive -project testCI/testCI.xcodeproj -scheme testCI -configuration Release -archivePath archive/testCI.xcarchive CODE_SIGN_IDENTITY="iPhone Developer"
+xcodebuild -exportArchive -archivePath archive/testCI.xcarchive -exportPath build  -exportOptionsPlist ./testCI/ExportOptions.plist
