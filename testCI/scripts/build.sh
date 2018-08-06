842c5d11ecdd4e7b9d42e237a9963e55b4b174cb
dujiepeng <347302029@qq.com>
2018-08-06 18:50:35 +0800

    update3

--- testCI/scripts/build.sh
+++ testCI/scripts/build.sh
@@ -1,1 +1,1 @@
-xcodebuild -project testCI.xcodeproj -target testCI -sdk iphonesimulator ONLY_ACTIVE_ARCH=NO
+xcodebuild -project testCI/testCI.xcodeproj -target testCI -sdk iphonesimulator ONLY_ACTIVE_ARCH=NO
