f74bc3b6edd138a0df4a5abde885e195e1b0c5d6
dujiepeng <347302029@qq.com>
2018-09-05 15:19:39 +0800

    33

--- testCI/build.sh
+++
@@ -1,9 +0,0 @@
-
-# xcodebuild archive -workspace easemob-OA.xcworkspace -scheme easemob-OA -configuration Release -archivePath archive/easemob-OA.xcarchive CODE_SIGN_IDENTITY="iPhone Developer"
-# xcodebuild -exportArchive -archivePath archive/easemob-OA.xcarchive -exportPath build  -exportOptionsPlist ./ExportOptions.plist
-
-
-
-
-xcodebuild archive -project testCI.xcodeproj -scheme testCI -configuration Release  -allowProvisioningUpdates -archivePath archive/testCI.xcarchive CODE_SIGN_IDENTITY="${DEVELOPER_NAME}" PROVISIONING_PROFILE="${PROFILE_NAME}"
-xcodebuild -exportArchive -archivePath archive/testCI.xcarchive -exportPath build  -exportOptionsPlist ./ExportOptions.plist -allowProvisioningUpdates
